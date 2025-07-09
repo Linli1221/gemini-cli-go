43277d4d695ada73d8e80a4882df94f8623d6727
linzero <root@linli.eu.org>
2025-07-09 22:10:56 +0800

    删除测试文件和脚本，清理不再使用的代码

--- test_api.ps1
+++
@@ -1,4 +0,0 @@
-$response = Invoke-WebRequest -Uri 'http://localhost:8080/v1/chat/completions' -Method POST -Headers @{'Content-Type'='application/json'; 'Authorization'='Bearer sk-1234567890abcdef1234567890abcdef'} -InFile 'test_request.json'
-Write-Host "Status Code: $($response.StatusCode)"
-Write-Host "Content:"
-$response.Content | ConvertFrom-Json | ConvertTo-Json -Depth 10
